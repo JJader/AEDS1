4dae33fc32001ad0b34820efac4992eb525e3129
JJader03 <jjader03@gmail.com>
2019-05-28 01:24:34 -0300

    Adicionando funcao excluir e liberar

--- Teste.c
+++ Teste.c
@@ -26,7 +26,7 @@ typedef struct Usuario
 {
     char nome[50];
     char sobrenome[50];
-    char CPF[15];
+    char CPF[2];
     char RG[9];
     TEndereco Endereco;
     TData Data;
@@ -61,6 +61,11 @@ void FVLazia(TLista *list){
 
 }
 
+int VerificaVazia(TLista list){
+    if(list.tam == 0){return 0;}
+    return 1;
+}
+
 void Inserir(TLista *list, Tusuario item){
 
     int a = list->tam;
@@ -71,25 +76,84 @@ void Inserir(TLista *list, Tusuario item){
     list->tam++;
 
     if(a < list->tam){
-        printf("Item inserido com Sucesso");
+        printf("\nItem inserido com Sucesso\n");
     }else{
-        printf("Ocorreu um Erro no processo");
+        printf("\nOcorreu um Erro no processo\n");
     }
 
 }
 
 Tcelula* Pesquisar(TLista *list, Tusuario item){
     Tcelula* i;
-    
+    if(list->tam == 0){return NULL;}
     for(i=list->primeira; i != NULL; i = i->prox){
-        if(strcmp(i->item.CPF, item.CPF) == 0){return i;}
+        if(strcmp(i->item.CPF, item.CPF) == 0){
+            return i;
+            }
     }
     return NULL;
 }
 
+void Excluir(TLista *list, Tusuario item){
+    Tcelula *cell, *aux;
+    cell = list->primeira;
+    
+    if(list->tam == 0){return;}
+        
+    while (cell->prox != NULL)
+    {
+        if ((strcmp(cell->prox->item.CPF, item.CPF) == 0))
+        {
+            aux = cell->prox;
+            cell->prox = cell->prox->prox;
+            free(aux); 
+            list->tam--; 
+            return;        
+        }
+            cell = cell->prox;
+    }
+
+    printf("\nNao ha o item\n");
+}
+
+void Liberar(TLista *list){
+    Tcelula *i;
+    
+    if(list->tam != 0 ){
+    while (list->primeira != NULL)
+    {
+        i = list->primeira->prox;
+        free(list->primeira);
+        list->primeira = i;
+    }
+    } 
+    else{
+        free(list->primeira);
+    }
+}
+
 void main(){
+    TLista lista;
+    Tusuario item;
+
+    FVLazia(&lista);
 
 
+    item.CPF[0] = '1';
+    Inserir(&lista, item);
+    item.CPF[0] = '2';
+    Inserir(&lista, item);
+    item.CPF[0] = '3';
+    Inserir(&lista, item);
 
+    Tcelula *i;
+    i = lista.primeira->prox;
 
+    for(i; i != NULL; i = i->prox){
+        printf("\n%s \n", i->item.CPF);
+    }
+    item.CPF[0] = '4';
+    Excluir(&lista, item);
+
+    Liberar(&lista);
 }
